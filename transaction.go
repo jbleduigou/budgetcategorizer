5004ea6388cf77faeb18ad826ea722bc4344eb24
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2019-12-19 21:35:30 +0100

    Adding data structure for transactions

---
+++ transaction.go
@@ -0,0 +1,15 @@
+package budget
+
+// Transaction is a transaction in the budget (purchase, refund).
+type Transaction struct {
+	Date        string
+	Description string
+	Comment     string
+	Category    string
+	Value       float64
+}
+
+// NewTransaction creates a transaction.
+func NewTransaction(date string, description string, comment string, category string, value float64) *Transaction {
+	return &Transaction{Date: date, Description: description, Comment: comment, Category: category, Value: value}
+}
