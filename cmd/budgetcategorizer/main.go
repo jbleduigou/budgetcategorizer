76c4cbbb8403d3a033828f9690cd373f9a2c74dd
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2019-12-20 17:08:22 +0100

    Extract logic to a command struct

--- cmd/budgetcategorizer/main.go
+++ cmd/budgetcategorizer/main.go
@@ -1,91 +1,36 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"regexp"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager/s3manageriface"
 	"github.com/jbleduigou/budgetcategorizer/exporter"
 	"github.com/jbleduigou/budgetcategorizer/parser"
 )
 
 func handler(ctx context.Context, s3Event events.S3Event) {
+	// Create all collaborators for command
 	sess := session.Must(session.NewSession())
 	downloader := s3manager.NewDownloader(sess)
 	uploader := s3manager.NewUploader(sess)
 	parser := parser.NewParser()
 	exporter := exporter.NewExporter()
 	for _, record := range s3Event.Records {
+		// Retrieve data from S3 event
 		s3event := record.S3
 		objectKey := strings.ReplaceAll(s3event.Object.Key, "input/", "")
-		execute(s3event.Bucket.Name, objectKey, downloader, uploader, parser, exporter)
+		// Instantiate a command
+		c := &command{s3event.Bucket.Name, objectKey, downloader, uploader, parser, exporter}
+		// Execute the command
+		c.execute()
 	}
 }
 
 func main() {
 	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
-}
-
-func execute(bucketName string, objectKey string, downloader s3manageriface.DownloaderAPI, uploader s3manageriface.UploaderAPI, p parser.Parser, e exporter.Exporter) {
-	//download file
-	content, _ := downloadFile(objectKey, bucketName, downloader)
-	//read transactions from file
-	transactions, _ := p.ParseTransactions(bytes.NewReader(content))
-	//write transactions to temp folder
-	resultFileName := getResultFileName(objectKey)
-	// writeResult(transactions, "/tmp/"+resultFileName)
-	output, _ := e.Export(transactions)
-	fmt.Printf("Output has size %v \n", len(output))
-	//	upload to s3
-	uploadResult(output, resultFileName, bucketName, uploader)
-}
-
-func downloadFile(objectKey string, bucketName string, downloader s3manageriface.DownloaderAPI) ([]byte, error) {
-	fmt.Printf("Downloading file '%v' from bucket '%v' \n", objectKey, bucketName)
-	buff := &aws.WriteAtBuffer{}
-	n, err := downloader.Download(buff, &s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String("input/" + objectKey),
-	})
-	if err != nil {
-		fmt.Printf("Failed to download file %v\n, %v", objectKey, err)
-		return nil, err
-	}
-	fmt.Printf("File %v downloaded, read %d bytes\n", objectKey, n)
-	return buff.Bytes(), nil
-}
-
-func getResultFileName(fileName string) string {
-	resultFileName := []byte(fileName)
-	re := regexp.MustCompile(`(\.CSV)`)
-	resultFileName = re.ReplaceAll(resultFileName, []byte("-result.txt"))
-	return string(resultFileName)
-}
-
-func uploadResult(result []byte, fileName string, bucketName string, uploader s3manageriface.UploaderAPI) (string, error) {
-	r := bytes.NewReader(result)
-	objectKey := "output/" + fileName
-
-	upParams := &s3manager.UploadInput{
-		Bucket: &bucketName,
-		Key:    &objectKey,
-		Body:   r,
-	}
-	o, err := uploader.Upload(upParams)
-	if err != nil {
-		return "", err
-	}
-	fmt.Printf("Success uploading file to location %v \n", o.Location)
-
-	return fileName, err
 }
