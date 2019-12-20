0bc037669ee64a739cb319389b991527b50af82c
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2019-12-20 09:41:41 +0100

    Adding unit test

---
+++ parser/parser_test.go
@@ -0,0 +1,61 @@
+package parser
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseTransactions(t *testing.T) {
+	p := NewParser()
+	transactions, _ := p.ParseTransactions(bytes.NewBufferString(content))
+	assert.Equal(t, 2, len(transactions))
+	debit := transactions[0]
+	assert.Equal(t, debit.Date, "18/12/2019")
+	assert.Equal(t, debit.Description, "Dans la vie on ne fait pas ce que l'on veut mais on est responsable de ce que l'on est. ")
+	assert.Equal(t, debit.Comment, "")
+	assert.Equal(t, debit.Category, "Courses Alimentation")
+	assert.Equal(t, debit.Value, 3.18)
+	credit := transactions[1]
+	assert.Equal(t, credit.Date, "18/12/2019")
+	assert.Equal(t, credit.Description, "3446335 Remise De Cheque ")
+	assert.Equal(t, credit.Comment, "")
+	assert.Equal(t, credit.Category, "")
+	assert.Equal(t, credit.Value, -30.13)
+}
+
+var content = `
+Téléchargement du  19/12/2019;
+
+  
+M. LE DUIGOU JEAN BAPTISTE    
+CCHQ       no 62734091867;
+Solde au 18/12/19 : 13,37 EUR
+
+Liste des opérations du compte entre le 18/12/2019 et le 18/12/2019;
+
+Date;Libellé;Débit Euros;Crédit Euros;
+18/12/2019;"Dans la vie on ne fait pas 
+ce que l'on veut 
+mais on est responsable 
+de ce que l'on est.
+";3,18;
+18/12/2019;"People acting together as a group 
+can accomplish things which no individual 
+acting alone could ever 
+hope to bring about.
+";not-a-number;
+18/12/2019;"3446335 
+Remise De Cheque 
+";;30,13;
+18/12/2019;"3446535 
+Remise De Cheque 
+";;not-a-number;
+
+  
+Mr Jb Le Duigou     
+VISA DUAL BZH DI no  4533 07xx xxxx xx60;
+
+;Pas d'opération pour cette carte entre le 18/12/2019 et 18/12/2019;
+`
