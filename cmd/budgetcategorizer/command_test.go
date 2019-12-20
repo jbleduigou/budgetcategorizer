9efb8ec820c53cfdfdb3026b40506ceb6bd43be9
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2019-12-20 21:58:55 +0100

    Move mock to a dedicated package

--- cmd/budgetcategorizer/command_test.go
+++ cmd/budgetcategorizer/command_test.go
@@ -1,46 +1,26 @@
 package main
 
 import (
-	"io"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/jbleduigou/budgetcategorizer/mock"
 	"github.com/stretchr/testify/assert"
 )
 
-type mockDownloader struct {
-	downloadCalled            bool
-	downloadWithContextCalled bool
-}
-
-func (_m *mockDownloader) Download(_a0 io.WriterAt, _a1 *s3.GetObjectInput, _a2 ...func(*s3manager.Downloader)) (int64, error) {
-	_m.downloadCalled = true
-	_a0.WriteAt([]byte("test"), 0)
-	return 1337, nil
-}
-
-func (_m *mockDownloader) DownloadWithContext(_a0 aws.Context, _a1 io.WriterAt, _a2 *s3.GetObjectInput, _a3 ...func(*s3manager.Downloader)) (int64, error) {
-	_m.downloadWithContextCalled = true
-	_a1.WriteAt([]byte("test"), 0)
-	return 1337, nil
-}
-
 func TestGetResultFileName(t *testing.T) {
-	c := &command{downloader: &mockDownloader{}}
+	c := &command{}
 
 	output := c.getResultFileName("CA20191220_1142.CSV")
 	assert.Equal(t, "CA20191220_1142-result.txt", output)
 }
 
 func TestDownloadFile(t *testing.T) {
-	m := &mockDownloader{}
+	m := mock.NewDownloader()
+	m.On("Download", mock.Anything, mock.Anything, mock.Anything).Return(int64(1337), nil)
 	c := &command{downloader: m}
 
 	content, err := c.downloadFile("CA20191220_1142.CSV", "mybucket")
 	assert.Equal(t, []byte("test"), content)
 	assert.Nil(t, err)
-	assert.Equal(t, true, m.downloadCalled)
-	assert.Equal(t, false, m.downloadWithContextCalled)
+	m.AssertExpectations(t)
 }
---
+++ mock/downloader.go
@@ -0,0 +1,39 @@
+package mock
+
+import (
+	"io"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	// Anything is used in Diff and Assert when the argument being tested
+	// shouldn't be taken into consideration.
+	Anything = "mock.Anything"
+)
+
+func NewDownloader() *MockDownloader {
+	return &MockDownloader{}
+}
+
+type MockDownloader struct {
+	mock.Mock
+}
+
+func (_m *MockDownloader) Download(_a0 io.WriterAt, _a1 *s3.GetObjectInput, _a2 ...func(*s3manager.Downloader)) (int64, error) {
+	ret := _m.Called(_a0, _a1, _a2)
+	_a0.WriteAt([]byte("test"), 0)
+	if ret.Get(1) != nil {
+		return ret.Get(0).(int64), ret.Get(1).(error)
+	}
+	return ret.Get(0).(int64), nil
+}
+
+func (_m *MockDownloader) DownloadWithContext(_a0 aws.Context, _a1 io.WriterAt, _a2 *s3.GetObjectInput, _a3 ...func(*s3manager.Downloader)) (int64, error) {
+	ret := _m.Called(_a0, _a1, _a2, _a3)
+	_a1.WriteAt([]byte("test"), 0)
+	return ret.Get(0).(int64), ret.Get(1).(error)
+}
