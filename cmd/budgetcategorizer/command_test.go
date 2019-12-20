5688492e3e4a12852e6b1dff244797e7dfb4df5e
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2019-12-20 22:02:50 +0100

    Use more specific args for mock

--- cmd/budgetcategorizer/command_test.go
+++ cmd/budgetcategorizer/command_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/jbleduigou/budgetcategorizer/mock"
 	"github.com/stretchr/testify/assert"
 )
@@ -16,7 +18,10 @@ func TestGetResultFileName(t *testing.T) {
 
 func TestDownloadFile(t *testing.T) {
 	m := mock.NewDownloader()
-	m.On("Download", mock.Anything, mock.Anything, mock.Anything).Return(int64(1337), nil)
+	m.On("Download",
+		mock.Anything,
+		&s3.GetObjectInput{Bucket: aws.String("mybucket"), Key: aws.String("input/CA20191220_1142.CSV")},
+		mock.Anything).Return(int64(1337), nil)
 	c := &command{downloader: m}
 
 	content, err := c.downloadFile("CA20191220_1142.CSV", "mybucket")
