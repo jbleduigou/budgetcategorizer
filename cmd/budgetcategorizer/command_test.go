d8a3bd9677827c170421072225655b298476438b
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2019-12-21 08:29:46 +0100

    Creating a mock for S3 uploader

--- cmd/budgetcategorizer/command_test.go
+++ cmd/budgetcategorizer/command_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/jbleduigou/budgetcategorizer/mock"
 	"github.com/stretchr/testify/assert"
 )
@@ -26,6 +27,17 @@ func TestDownloadFile(t *testing.T) {
 
 	content, err := c.downloadFile("CA20191220_1142.CSV", "mybucket")
 	assert.Equal(t, []byte("test"), content)
+	assert.Nil(t, err)
+	m.AssertExpectations(t)
+}
+
+func TestUploadFile(t *testing.T) {
+	m := mock.NewUploader()
+	m.On("Upload", mock.Anything, mock.Anything).Return(&s3manager.UploadOutput{Location: ""}, nil)
+	c := &command{uploader: m}
+
+	fileName, err := c.uploadResult([]byte("test"), "CA20191220_1142-result.txt", "mybucket")
+	assert.Equal(t, "CA20191220_1142-result.txt", fileName)
 	assert.Nil(t, err)
 	m.AssertExpectations(t)
 }
