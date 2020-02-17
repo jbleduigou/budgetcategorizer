ad2872962eda7d8807674f6466d505ae39248982
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2020-02-17 20:51:20 +0100

    Update CI pipeline
    Signed-off-by: Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -3,29 +3,65 @@ pipeline {
     agent {
         docker { image 'golang:1.13' }
     }
+    environment {
+        XDG_CACHE_HOME='/tmp/.cache'
+        GOOS='linux'
+        GOARCH='amd64'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                // Get the code from GitHub repository
+                git 'https://github.com/jbleduigou/budgetcategorizer.git'
 
-   stages {
-      stage('Build') {
-         steps {
-            // Get the code from GitHub repository
-            git 'https://github.com/jbleduigou/budgetcategorizer.git'
-            sh 'mkdir -p $GOPATH/src/github/jbleduigou'
-            sh 'ln -s $WORKSPACE $GOPATH/src/github/jbleduigou/budgetcategorizer'
+                // Code is checked out in a separate folder, create symlink to GOPATH
+                sh 'mkdir -p $GOPATH/src/github/jbleduigou'
+                sh 'ln -s $WORKSPACE $GOPATH/src/github/jbleduigou/budgetcategorizer'
 
-            // Run Maven on the amazon linux agent.
-            sh 'export XDG_CACHE_HOME=/tmp/.cache && cd $GOPATH/src/github/jbleduigou/budgetcategorizer && go build -o budgetcategorizer ./cmd/budgetcategorizer'
-         }
-      }
-      stage('Test') {
-         steps {
-            sh 'export XDG_CACHE_HOME=/tmp/.cache && cd $GOPATH/src/github/jbleduigou/budgetcategorizer && make test'
-         }
-      }
-      stage('Linting') {
-         steps {
-            sh 'export XDG_CACHE_HOME=/tmp/.cache && go get -u golang.org/x/lint/golint'
-            sh 'export XDG_CACHE_HOME=/tmp/.cache && cd $GOPATH/src/github/jbleduigou/budgetcategorizer && go list ./... | grep -v /vendor/ | xargs -L1 golint -set_exit_status'
-         }
-      }
-   }
+                // Build the go project
+                sh 'cd $GOPATH/src/github/jbleduigou/budgetcategorizer && go build -o budgetcategorizer ./cmd/budgetcategorizer'
+            }
+            post {
+                success {
+                    archiveArtifacts artifacts: 'budgetcategorizer', fingerprint: true
+                }
+            }
+        }
+        stage('Test') {
+            steps {
+                // Retrieve tool for converting output to junit format
+                sh 'go get -u github.com/jstemmer/go-junit-report'
+                
+                // sh "sed -i -e 's|???|N/A|g' categorizer/categorizer.go"
+
+                // Run unit tests and redirect output to go-junit-report
+                sh 'cd $GOPATH/src/github/jbleduigou/budgetcategorizer && go test -v ./... 2>&1 | go-junit-report > report.xml'
+            }
+            post {
+                always {
+                  // Publish test results
+                  step([$class: 'JUnitResultArchiver', testResults: 'report.xml'])
+                }
+            }
+        }
+        stage('Violations') {
+            steps {
+                // Retrieve golint tool
+                sh 'go get -u golang.org/x/lint/golint'
+                sh "sed -i -e 's|NewBroker will|this will|g' messaging/message.go"
+
+                // Run golint
+                sh 'cd $GOPATH/src/github/jbleduigou/budgetcategorizer && golint ./...'
+
+                // Run go vet 
+                sh "sed -i -e 's|objectKey, bucket|objectKey, 1337|g' config/config.go"
+                sh 'cd $GOPATH/src/github/jbleduigou/budgetcategorizer && go vet ./... || true'
+            }
+            post {
+              always {
+                recordIssues enabledForFailure: true, tool: goLint(), qualityGates: [[threshold: 1, type: 'TOTAL', unstable: true]]
+              }
+            }
+        }
+    }
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,10 @@
 module github.com/jbleduigou/budgetcategorizer
 
 require (
-	github.com/aws/aws-lambda-go v1.13.3
-	github.com/aws/aws-sdk-go v1.28.9
+	github.com/aws/aws-lambda-go v1.14.0
+	github.com/aws/aws-sdk-go v1.29.3
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.4.0
-	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 	gopkg.in/yaml.v2 v2.2.8
 )
 
