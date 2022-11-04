0a325c7de14cdeb19e10914306b09f7bbdce36c1
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2022-11-04 09:47:10 +0100

    👷 update to go 1.19

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 pipeline {
     agent {
-        docker { image 'golang:1.17' }
+        docker { image 'golang:1.19' }
     }
     environment {
         XDG_CACHE_HOME='/tmp/.cache'
