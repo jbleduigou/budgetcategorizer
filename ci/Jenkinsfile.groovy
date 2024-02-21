cd79ba5078ade480749b41e575ab870264384949
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2024-02-21 20:10:51 +0000

    chore: ⬆ update to Go 1.22

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 pipeline {
     agent {
-        docker { image 'golang:1.21' }
+        docker { image 'golang:1.22' }
     }
     environment {
         XDG_CACHE_HOME='/tmp/.cache'
